4d39458af02bc4b89a4f024004ab74dab4d238f7
Xavi Duran <xavier.duran@pm.me>
2019-09-06 13:08:32 +0200

    Set initial working version (#1)

--- README.md
+++ README.md
@@ -2,16 +2,24 @@
 
 ## Docker
 
-A docker image with all software, libraries and configuration needed is provided
-
-### Build docker image
+A Docker image with all software, libraries and configuration needed is provided. To build the Docker image run
 
 ```sh
-sudo docker-compose build deanonymize
+$ sudo docker-compose build deanonymize
 ```
 
-## Start system on docker
+Then run the Docker image.
 
 ```sh
-sudo docker-compose run deanonymize
+$ sudo docker-compose run deanonymize
 ```
+
+## Deanonymize and generate pdf
+
+### Usage
+
+```sh
+$ python src/run.py --report output/697~201905.html --data data/sample.csv --layout layout/landscape.json
+```
+
+output files `output/697~deanonymized~201905.html` and `output/697~deanonymized~201905.pdf` will be generated.
--- src/run.py
+++ src/run.py
@@ -1,12 +1,23 @@
 import optparse
+import json
+
 from utils_report import UtilsReport
+from deanonymize_html import DeanonymizeHTML
 
 
 if __name__ == "__main__":
 
     parser = optparse.OptionParser()
-    parser.add_option('-c', '--contracts', dest='contracts', help='Contracts')
+
+    parser.add_option('-d', '--data', dest='data', help='Deanonymized data filename')
+    parser.add_option('-l', '--layout', dest='layout', help='PDF layout')
+    parser.add_option('-r', '--report', dest='report', help='Report filename')
 
     (options, args) = parser.parse_args()
 
-    UtilsReport.write_pdf_landscape('697', 201905)
+    # Deanonymize report
+    output_filename = DeanonymizeHTML.replace_html(options.report, options.data)
+
+    # Generate PDF
+    with open(options.layout) as json_file:
+        UtilsReport.write_pdf_landscape(output_filename, json.load(json_file))
