8bff592ff6bb4a90378890910f3abe112a4d039c
Xavier Duran <xavier.duran@gmail.com>
2019-09-10 15:57:23 +0200

    wdswdsd

--- src/deanonymize_html.py
+++ src/deanonymize_html.py
@@ -3,19 +3,18 @@ import csv
 
 
 class DeanonymizeHTML(object):
-
     @staticmethod
     def replace_html(filename, data):
         """Replaces anonymized tokens in a given file with real data."""
         with open(data, mode='r') as infile:
             csv_reader = csv.DictReader(infile)
             translations = {
-                '%s_%s' % (cell, row['id']): row[cell] for row in csv_reader for cell in row.keys()
+                '%s_%s' % (cell, row['id']): row[cell]
+                for row in csv_reader for cell in row.keys()
             }
         with open(filename, 'r') as anonymized_html:
             anonymized_template = string.Template(anonymized_html.read())
             with open(filename.replace('~', '~deanonymized~'), 'w') as outfile:
                 outfile.write(anonymized_template.substitute(translations))
-weweweew
-fdfdf
+        print "swswdsd"
         return outfile.name
