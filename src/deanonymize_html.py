ff3449dc27fb9a34ebb2a84f40dba7b519a172b5
Xavier Duran <xavier.duran@gmail.com>
2019-09-20 12:51:45 +0200

    safe_substitute

--- src/deanonymize_html.py
+++ src/deanonymize_html.py
@@ -15,5 +15,6 @@ class DeanonymizeHTML(object):
         with open(filename, 'r') as anonymized_html:
             anonymized_template = string.Template(anonymized_html.read())
             with open(filename.replace('~', '~deanonymized~'), 'w') as outfile:
-                outfile.write(anonymized_template.substitute(translations))
+                outfile.write(
+                    anonymized_template.safe_substitute(translations))
         return outfile.name
