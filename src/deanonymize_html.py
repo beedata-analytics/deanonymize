26d1ccafee748ceb9318d9dbc60fc2d8da6024fd
Xavier Duran <xavier.duran@gmail.com>
2020-09-17 12:22:20 +0200

    Update to ubuntu 18.04 and python3

--- src/deanonymize_html.py
+++ src/deanonymize_html.py
@@ -6,15 +6,17 @@ class DeanonymizeHTML(object):
     @staticmethod
     def replace_html(filename, data):
         """Replaces anonymized tokens in a given file with real data."""
-        with open(data, mode='r') as infile:
+        with open(data, mode="r") as infile:
             csv_reader = csv.DictReader(infile)
             translations = {
-                '%s_%s' % (cell, row['id']): row[cell]
-                for row in csv_reader for cell in row.keys()
+                "%s_%s" % (cell, row["id"]): row[cell]
+                for row in csv_reader
+                for cell in row.keys()
             }
-        with open(filename, 'r') as anonymized_html:
+        with open(filename, "r") as anonymized_html:
             anonymized_template = string.Template(anonymized_html.read())
-            with open(filename.replace('~', '~deanonymized~'), 'w') as outfile:
+            with open(filename.replace("~", "~deanonymized~"), "w") as outfile:
                 outfile.write(
-                    anonymized_template.safe_substitute(translations))
+                    anonymized_template.safe_substitute(translations)
+                )
         return outfile.name
--- src/utils_report.py
+++ src/utils_report.py
@@ -1,5 +1,5 @@
 import pdfkit
-import os
+from os import path
 
 
 class UtilsReport(object):
@@ -7,12 +7,13 @@ class UtilsReport(object):
     def write_pdf_landscape(filename, options, output_dir):
         """Converts html to a landscape pdf."""
         try:
-            output = os.path.join(output_dir,
-                                  os.path.basename(filename)).replace(
-                                      'html', 'pdf')
+            output = path.join(output_dir, path.basename(filename)).replace(
+                "html", "pdf"
+            )
             pdfkit.from_file(str(filename), str(output), options=options)
-        except Exception, e:
-            raise Exception('''Report in pdf for file %s was not generated
-                due to problems in the write_pdf function. Error: %s''' %
-                            (filename, e))
+        except Exception as e:
+            raise Exception(
+                "Report in pdf for file %s was not generated due to problems "
+                "in the write_pdf function. Error: %s" % (filename, e)
+            )
         return output
