0f4e34a3190d27920b252d04d2aff92538db37e4
Gemma Argelés <gargeles@hotmail.com>
2023-08-11 16:10:05 +0200

    Improve runtime performance

--- src/deanonymize_html.py
+++ src/deanonymize_html.py
@@ -1,11 +1,7 @@
-import codecs
-import csv
 import os
 import shutil
 import subprocess
-import unicodedata
-from ast import parse
-
+from utils_report import UtilsReport
 
 class DeanonymizeHTML(object):
 
@@ -18,49 +14,18 @@ class DeanonymizeHTML(object):
 
 
     @staticmethod
-    def replace_html(filename, target, data, delimiter, id, batch_text):
+    def replace_html(filename, target, data):
         """fix the contract name """
-
-        def valid_variable_name(name):
-            try:
-                parse("{} = None".format(name))
-                return name
-            except Exception:
-                # general case
-                # new_name = hashlib.md5(name).hexdigest()
-                # only considering the enercoop case
-                new_name = name.replace("-", "_")
-                return new_name
 
         """Replaces anonymized tokens in a given file with real data."""
 
         shutil.copyfile(filename, target)
 
-        with codecs.open(data, mode="r", encoding="utf-8") as infile:
-            csv_reader = csv.DictReader(infile, delimiter=delimiter)
-            translations = {
-                # avoiding special characters and replacing
-                # blank spaces with '_'
-                "%s_%s"
-                % (
-                    unicodedata.normalize("NFKD", cell)
-                    .encode("ascii", "ignore")
-                    .decode("ascii")
-                    .replace(" ", "_"),
-                    valid_variable_name(row[id]),
-                ): row[cell]
-                .encode("ascii", "xmlcharrefreplace")
-                .decode("ascii")
-                for row in csv_reader
-                for cell in row.keys()
-            }
-            contract = valid_variable_name(
-                os.path.basename(filename).split("~")[0]
-            )
+        contract = UtilsReport.valid_variable_name(
+            os.path.basename(filename).split("~")[0]
+        )
 
-            translations = {
-                k: translations[k] for k in translations if contract in k
-            }
+        translations = data.get(contract)
 
         for key in translations:
             subprocess.call(
